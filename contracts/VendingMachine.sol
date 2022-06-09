601ad9b3432aee616008afa6a50ecf071e08f2b4
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2022-06-09 15:14:51 -0300

    Sell product feature

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,17 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract VendingMachine {
+    uint totalProducts;
     address private owner;
-    Product[] private repository;
+    Product[] public repository;
 
     struct Product {
+        uint id;
         string name;
         uint quantity;
         uint price;
     }
 
-    event productAdded(string productName);
+    event added(string productName);
+    event sold(string productName, uint amount, address buyer);
 
     modifier OnlyOwner {
         require(msg.sender == owner, "Only owner should do this.");
@@ -20,11 +25,26 @@ contract VendingMachine {
 
     constructor() {
         owner = msg.sender;
+        totalProducts = 0;
     }
 
-    function addProduct(string calldata name, uint price) public OnlyOwner {
-        Product memory newProduct = Product(name, 5, price);
+    function add(string calldata name, uint price) public OnlyOwner {
+        totalProducts++;
+        Product memory newProduct = Product(totalProducts, name, 5, price);
         repository.push(newProduct);
-        emit productAdded(name);
+        emit added(name);
+    }
+
+    function sell(uint id, uint amount) public {
+        for (uint i = 0; i < repository.length; i++) {
+            Product memory product = repository[i];
+            if (product.id == id) {
+                require(amount <= product.quantity, "Try to buy more then available.");
+                repository[i].quantity -= amount;
+                emit sold(product.name, amount, msg.sender);
+                return ;
+            }
+        }
+        revert("Product not found.");
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -11,16 +11,45 @@ describe("VendingMachine", function () {
     });
 
     it("Should add a product.", async () => {
-        await expect(vending.addProduct("Sprite Soda", 2))
+        await expect(vending.add("Sprite Soda", 2))
             .to
-            .emit(vending, "productAdded");
+            .emit(vending, "added");
     });
 
     it("Should fail to add a product when address different from owner.", async () => {
         const accounts = await ethers.getSigners();
-        await expect(vending.connect(accounts[1]).addProduct("Sprite Soda", 2))
+        await expect(vending.connect(accounts[1]).add("Sprite Soda", 2))
             .to
             .be
             .revertedWith("Only owner should do this.");
+    })
+
+    it("Should sell a product.", async () => {
+        await expect(vending.add("Sprite Soda", 2))
+            .to
+            .emit(vending, "added");
+
+        await expect(vending.sell(1, 1))
+            .to
+            .be
+            .emit(vending, "sold");
+    });
+
+    it("Should revert with not found when try to buy a non-existent product.", async () => {
+        await expect(vending.sell(42, 1))
+            .to
+            .be
+            .revertedWith("Product not found.");
+    });
+
+    it("Should revert when try to buy more products then available.", async () => {
+        await expect(vending.add("Sprite Soda", 2))
+        .to
+        .emit(vending, "added");
+        
+        await expect(vending.sell(1, 6))
+            .to
+            .be
+            .revertedWith("Try to buy more then available.");
     })
 })
