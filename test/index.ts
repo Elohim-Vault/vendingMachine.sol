2480d85f90c576db4a6f2169aac102eeb4501a6f
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2022-06-13 14:53:10 -0300

    Improve tests description

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { VendingMachine } from "../typechain";
 import { expect } from "chai";
 
-describe("VendingMachine", function () {
+describe("Vending machine smart contract tests", function () {
     let vending: VendingMachine;
     beforeEach(async () => {
         const Vending = await ethers.getContractFactory("VendingMachine");
@@ -16,7 +16,7 @@ describe("VendingMachine", function () {
             .emit(vending, "added");
     });
 
-    it("Should fail to add a product when address different from owner.", async () => {
+    it("Should revert when to try to add a product with address different from the owner.", async () => {
         const accounts = await ethers.getSigners();
         await expect(vending.connect(accounts[1]).add("Sprite Soda", 2))
             .to
@@ -35,14 +35,14 @@ describe("VendingMachine", function () {
             .emit(vending, "sold");
     });
 
-    it("Should revert with not found when try to buy a non-existent product.", async () => {
+    it("Should revert with not found when to try to buy a non-existent product.", async () => {
         await expect(vending.buy(42, 1))
             .to
             .be
             .revertedWith("Product not found.");
     });
 
-    it("Should revert when try to buy more products then available.", async () => {
+    it("Should revert when to try to buy more products than available.", async () => {
         await expect(vending.add("Sprite Soda", 2))
         .to
         .emit(vending, "added");
@@ -53,7 +53,7 @@ describe("VendingMachine", function () {
             .revertedWith("Try to buy more then available.");
     });
 
-    it("Should revert when send less wei then product price.", async () => {
+    it("Should revert when to send less wei than the product price.", async () => {
         await expect(vending.add("Sprite Soda", 2))
         .to
         .emit(vending, "added");
